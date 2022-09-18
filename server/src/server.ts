80664c06b50d2233796a2494c43cb49d339d4230
Reginaldo Sena <regsena@gmail.com>
2022-09-17 23:14:49 -0400

    installed vite

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,8 @@ app.get('/ads', (req, res) => {
         {id:1, name: 'Anúncio 1'},
         {id:2, name: 'Anúncio 2'},
         {id:3, name: 'Anúncio 3'},
-        {id:4, name: 'Anúncio 4'}
+        {id:4, name: 'Anúncio 4'},
+        {id:5, name: 'Anúncio 5'}
       ]); 
 });
 
